59cc03524bf13bf2b868f618168ea1f557d501b1
imriteshvanjare2086 <iriteshvanjare20@gmail.com>
2026-01-06 22:14:18 +0530

    Add Single-Level Inheritance

---
+++ Inheritance/Single-Level.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person
+{
+protected:
+    string name;
+    int age;
+    string department;
+
+public:
+    Person(string name, int age, string department)
+    {
+        this->name = name;
+        this->age = age;
+        this->department = department;
+    }
+};
+
+class Student : public Person   
+{
+    int roll_no;
+
+public:
+    Student(string name, int age, string department, int roll_no)
+        : Person(name, age, department)
+    {
+        this->roll_no = roll_no;
+    }
+
+    void display()
+    {
+        cout << name << endl;
+        cout << age << endl;
+        cout << department << endl;
+        cout << roll_no << endl;
+    }
+};
+
+int main()
+{
+    Student s1("Ritesh", 19, "IT", 149);
+    s1.display();
+}
+
+// Ritesh
+// 19
+// IT
+// 149
+
+
