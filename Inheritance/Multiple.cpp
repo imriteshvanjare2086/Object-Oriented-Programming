e7eea2a24804feeb8224d569640e3152fd6c08d7
imriteshvanjare2086 <iriteshvanjare20@gmail.com>
2026-01-15 18:34:25 +0530

    Add multiple inheritance

--- Inheritance/Multiple.cpp
+++ Inheritance/Multiple.cpp
@@ -2,87 +2,80 @@
 #include <string>
 using namespace std;
 
-class Parent
+class Parent1
 {
-protected:
-    string name;
-    int age;
-    string gender;
-    float salary;
-
-public:
-    Parent(string name, int age, string gender, float salary)
+  protected:
+  string name;
+  int age;
+  
+  public:
+  Parent1(string name, int age)
     {
-        this->name = name;
-        this->age = age;
-        this->gender = gender;
-        this->salary = salary;
+      this->name = name;
+      this->age = age;
     }
-
+    
     virtual void display()
     {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-        cout << "Gender: " << gender << endl;
-        cout << "Salary: " << salary << endl;
-        cout << endl;
+      cout << "Name: " << name << endl;
+      cout << "Age: " << age << endl;
+      cout << endl;
     }
-};
-
-class Child1 : public Parent
-{
-protected:
-    string hobby;
-
-public:
-    Child1(string name, int age, string gender, float salary, string hobby)
-        : Parent(name, age, gender, salary)
+  };
+  
+  class Parent2
+  {
+    protected:
+    string gender;
+    float salary;
+    
+    public:
+    Parent2(string gender, float salary)
     {
-        this->hobby = hobby;
+      this->gender = gender;
+      this->salary = salary;
     }
-
+    
+    virtual void display()
+    {
+      cout << "Gender: " << gender << endl;
+      cout << "Salary: " << salary << endl;
+      cout << endl;
+    }
+  };
+  
+  class Child : public Parent1, public Parent2
+  {
+    protected:
+    string hobby;
+    
+    public:
+    Child(string name, int age, string gender, float salary, string hobby)
+    : Parent1(name, age), Parent2(gender, salary)
+    {
+      this->hobby = hobby;
+    }
+    
     void display() override
     {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-        cout << "Gender: " << gender << endl;
+      cout << "Name: " << name << endl;
+      cout << "Age: " << age << endl;
+      cout << "Gender: " << gender << endl;
         cout << "Hobby: " << hobby << endl;
         cout << endl;
-    }
-};
-
-class Child2 : public Parent
+      }
+    };
+    
+    int main()
 {
-protected:
-    string hobby;
-
-public:
-    Child2(string name, int age, string gender, float salary, string hobby)
-        : Parent(name, age, gender, salary)
-    {
-        this->hobby = hobby;
-    }
-
-    void display() override
-    {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-        cout << "Gender: " << gender << endl;
-        cout << "Hobby: " << hobby << endl;
-        cout << endl;
-    }
-};
-
-int main()
-{
-    Parent p1("Sanjay", 52, "Male", 500000.0);
+  Parent1 p1("Sanjay", 52);
     p1.display();
 
-    Child1 c1("Ritesh", 19, "Male", 10000.0, "Cricket");
-    c1.display();
+    Parent2 p2("Male", 100000.0);
+    p2.display();
 
-    Child1 c2("Shaizaan", 19, "Male", 20000.0, "Gaming");
-    c2.display();
+    Child c1("Ritesh", 19, "Male", 200000.0, "Cricket");
+    c1.display();
 
     return 0;
 }
